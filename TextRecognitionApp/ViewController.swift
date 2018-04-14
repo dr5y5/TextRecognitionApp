97b9b64d794bb261b79051b43c6510caf0315811
seoju <wngus606@gmail.com>
2018-04-14 16:00:50 +0900

    add caputre session function

--- TextRecognitionApp/ViewController.swift
+++ TextRecognitionApp/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession = AVCaptureSession()
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        checkCameraPremission { (granted) in
+            self.configureCamera()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+// MARK: - Configuration
+extension ViewController {
+
+    func configureCamera() {
+        
+        guard let captureDevice: AVCaptureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back) else { return }
+        
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            if captureSession.canAddInput(deviceInput) {
+                captureSession.addInput(deviceInput)
+            }
+            captureSession.sessionPreset = .high
+            
+        } catch {
+            print("DeviceInput error")
+            return
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer?.frame = self.view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession.startRunning()
+    }
+    
+    func configureTextDetection() {
+        
+    }
+    
+    func checkCameraPremission(_ handler: @escaping (_ granted: Bool) -> Void) {
+        func hasCameraPermission() -> Bool {
+            return AVCaptureDevice.authorizationStatus(for: .video) == .authorized
+        }
+        
+        func needsToRequestCameraPermission() -> Bool {
+            return AVCaptureDevice.authorizationStatus(for: .video) == .notDetermined
+        }
+        
+        hasCameraPermission() ? handler(true) : (needsToRequestCameraPermission() ?
+            AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted) in
+                DispatchQueue.main.async {
+                    hasCameraPermission() ? handler(true) : handler(false)
+                }
+            }) : handler(false))
+    }
+}
+
+// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+}
